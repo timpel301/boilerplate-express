6be6d893edb0a04113eb724bc2ddf24ca840d479
Tim.Pelzer@external.telekom.de <Tim.Pelzer@external.telekom.de>
2023-06-08 23:50:39 +0200

    add query example using name route

--- myApp.js
+++ myApp.js
@@ -36,6 +36,10 @@ app.get('/:word/echo', function(req,res){
     res.json({"echo": req.params.word});
 });
 
+app.route('/name').get(function(req,res){
+    res.json({"name": req.query.first+" "+req.query.last})
+});
+
 
 
 
