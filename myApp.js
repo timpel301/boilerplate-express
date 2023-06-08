94927b31e6e0b56460c038df39a09df70e01f6e9
Tim.Pelzer@external.telekom.de <Tim.Pelzer@external.telekom.de>
2023-06-08 22:53:26 +0200

    add .env var and condition

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 console.log('Hello World');
 
@@ -10,7 +11,13 @@ app.get('/', function(req, res) {
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', function(req, res) {
-    res.json({"message": "Hello json"})
+    process.env.MESSAGE_STYLE
+    if(process.env.MESSAGE_STYLE==uppercase){
+        res.json({"message": "HELLO JSON"})
+    }
+    else{
+        res.json({"message": "Hello json"})
+    };
 });
 
 
